59cc83fb6b52cf5046ba6e9f5e51a23968c9734e
hikettei <hikettei@gmail.com>
2025-06-09 23:07:16 +0900

    react to mention

--- sfc_bot/bot.py
+++ sfc_bot/bot.py
@@ -9,14 +9,15 @@ TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
 intents = discord.Intents.default()
 
-bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
-
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"),
+    intents=intents,
+    help_command=None,
+    activity=discord.Game("<@mention> help"))
 
 @bot.command(name="ping")
 async def ping(ctx: commands.Context):
     """Respond with pong."""
     await ctx.send("Pong!")
-
 
 @bot.command(name="help")
 async def help_command(ctx: commands.Context):
@@ -24,10 +25,8 @@ async def help_command(ctx: commands.Context):
     help_text = "!ping: Respond with 'Pong!'\n!help: Show this help message."
     await ctx.send(help_text)
 
-
 def main() -> None:
     bot.run(TOKEN)
-
 
 if __name__ == "__main__":
     main()
